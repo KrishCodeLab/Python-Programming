4a84fb123171f3e034d89d4a63cdc42296db7f7e
Krushna Govardhane <krushnagoverdhane@gmail.com>
2026-09-06 00:32:51 +0530

    Learned about Functions in python and solved Basic Arithmetic Operations using function

---
+++ Functions.py
@@ -0,0 +1,20 @@
+# Function : A function is a reusable block of code designed to perform a particular task. Functions help make programs organized, reusable, and easier to understand
+
+# Creating function to Say hii to ourself
+def hello():
+  print("Hello Krish...")
+
+hello()
+
+# Creating function to say Hello to everyone
+def sayHello():
+  print("Hello!!!")
+
+# Creating function to say Namaste to everyone
+def sayNamaste():
+  print("Namaste!!!")
+
+
+# Calling function to do work
+sayHello()
+sayNamaste()
