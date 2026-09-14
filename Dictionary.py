b698d4905328263ba21f3edc8911904ab920caba
Krushna Govardhane <krushnagoverdhane@gmail.com>
2026-09-14 17:26:51 +0530

    Learned about the Dictionary

---
+++ Dictionary.py
@@ -0,0 +1,11 @@
+# Dictionary in python : A dictionary is a Python data structure used to store data in key-value pairs
+
+student = {
+    "name": "Krish",
+    "age": 20,
+    "course": "AI"
+}
+print(student)
+print(student["name"])
+
+print(student["age"])
