b2ae42101989ad35d7eb76cfcaf6377c1115e0c5
Krushna Govardhane <krushnagoverdhane@gmail.com>
2026-09-22 22:33:09 +0530

    String Method Question : Enter your college name and emial and convert it to Lower case

---
+++ String Methods/ConvertToLower.py
@@ -0,0 +1,6 @@
+# Enter your college name and email and convert it to lower case
+college_name=input("Enter your college name ")
+email=input("Enter your email ")
+
+print(college_name.lower())
+print(email.lower())
