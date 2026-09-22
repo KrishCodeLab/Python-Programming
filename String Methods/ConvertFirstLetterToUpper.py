6c330163ed88a29ee273aaaec2d7faea5489a0c4
Krushna Govardhane <krushnagoverdhane@gmail.com>
2026-09-22 22:39:33 +0530

    String Method Question : Enter 3 random sentences and print in same line and capitialize first letter of every first letter of each sentence

---
+++ String Methods/ConvertFirstLetterToUpper.py
@@ -0,0 +1,7 @@
+# Enter any random sentence and convert starting of the sentence 
+sentence1=input("Enter Sentence : ")
+sentence2=input("Enter Sentence : ")
+sentence3=input("Enter Sentence : ")
+
+
+print(sentence1.capitalize(),".",sentence2.capitalize(),".",sentence3.capitalize())
