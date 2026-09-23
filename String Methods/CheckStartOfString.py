56ba53883b92da5619b1392c7ad6dc546f2f4f24
Krushna Govardhane <krushnagoverdhane@gmail.com>
2026-09-24 00:15:18 +0530

    String Method Question : Check Start of String

---
+++ String Methods/CheckStartOfString.py
@@ -0,0 +1,6 @@
+# Check whether a string starts with "Hello" using startswith().
+s=input("Enter string : ")
+# startswith():Returns true if the starting of string is equal to parameter passsed to method else False
+check=input("Enter to check start of string")
+
+print(s.startswith(check))
