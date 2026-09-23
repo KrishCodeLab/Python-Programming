03c8332265edf6c3c853f9e118bd789fa98b19ee
Krushna Govardhane <krushnagoverdhane@gmail.com>
2026-09-23 23:58:37 +0530

    String Method Question : Find Word in String

---
+++ String Methods/FindWordInString.py
@@ -0,0 +1,4 @@
+# Find the position of "python" using find().
+Random_sentence=input("Enter Sentence")
+# find():- Returns the first index of word if the word exists in string else return -1
+print(Random_sentence.find("Python"))
