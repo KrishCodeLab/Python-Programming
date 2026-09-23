355984b0bd525fd5fbc008bf88395667759951f1
Krushna Govardhane <krushnagoverdhane@gmail.com>
2026-09-24 00:21:29 +0530

    Check Start of String question changed by adding Check end also

--- String Methods/CheckStartEndOfString.py
+++ String Methods/CheckStartEndOfString.py
@@ -1,6 +1,14 @@
 # Check whether a string starts with "Hello" using startswith().
+# Check whether a string ends with ".com" using endswith().
 s=input("Enter string : ")
 # startswith():Returns true if the starting of string is equal to parameter passsed to method else False
+# endswith():Return true if the ending of string is equal to parameter that is passed to method else False
 check=input("Enter to check start of string")
 
 print(s.startswith(check))
+print(s.startswith("Hello"))
+
+print(s.endswith(" "))
+print(s.endswith("Bye"))
+
+
